731a766a1384a93eafa3bfa0202b29d39edcb909
lyne18-8-meet <lyneshweiki@gmail.com>
2018-01-30 20:07:09 +0200

    database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///./test.db'
 
 @app.route('/')
 def index():
@@ -12,19 +12,34 @@ db=SQLAlchemy(app)
 class Users(db.Model):
 	__tablename__="Users"
 	id = db.Column(db.Integer, primary_key=True)
-	username=db.Column('username',db.String)
-	password=db.Column('password',db.String)
+	username=db.Column(db.String(30))
+	password=db.Column(db.String(30))
+
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+
 db.create_all()
 
 @app.route('/submit_form',methods=['POST'])
 def submit_form():
-	username = request.form['username']
+	username = request.form['email']
+	password = request.form['psw']
+	users = Users.query.filter_by(username=username).first()
+	if password == users.password:
+		return render_template("home.html")
+	else:
+		print('password did not match')
 
 @app.route('/signup_form',methods=['POST'])
 def signup_form():
-	username = request.form['username']
-
-users = Users.query.all()
+	username = request.form['email']
+	password = request.form['psw']
+	x = Users(username,password)
+	db.session.add(x)
+	db.session.commit()
+	return render_template("home.html")
+    
 
 if __name__ == "__main__":
 	app.run()
